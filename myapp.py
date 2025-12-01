f4d090e5978f04588e9a1c5129dea90ba977e65d
nguyenhuuthanh2k6-wed <nguyenhuuthanh2k6@gmail.com>
2025-12-02 00:18:40 +0700

    hhjhj

--- myapp.py
+++ myapp.py
@@ -1,17 +1,34 @@
-def is_triangle(a, b, c):
-    return (
-        a > 0 and b > 0 and c > 0 and
-        a + b > c and
-        a + c > b and
-        b + c > a
-    )
-
-print("Kiểm tra tam giác")
 a = float(input("Nhập cạnh a: "))
 b = float(input("Nhập cạnh b: "))
 c = float(input("Nhập cạnh c: "))
 
-if is_triangle(a, b, c):
-    print("✔ Đây là 3 cạnh của một tam giác.")
+# Kiểm tra có phải tam giác không
+if a + b > c and a + c > b and b + c > a:
+    print("Đây là 3 cạnh của một tam giác")
+
+    # Tam giác đều
+    if a == b == c:
+        print("→ Tam giác đều")
+
+    # Tam giác vuông
+    elif a*a + b*b == c*c or a*a + c*c == b*b or b*b + c*c == a*a:
+        # Vuông cân
+        if a == b or a == c or b == c:
+            print("→ Tam giác vuông cân")
+        else:
+            print("→ Tam giác vuông")
+
+    # Tam giác cân
+    elif a == b or a == c or b == c:
+        print("→ Tam giác cân")
+
+    # Tam giác nhọn
+    elif (a*a + b*b > c*c) and (a*a + c*c > b*b) and (b*b + c*c > a*a):
+        print("→ Tam giác nhọn")
+
+    # Tam giác tù
+    else:
+        print("→ Tam giác tù")
+
 else:
-    print("✘ Không phải là tam giác.")
+    print("Không phải 3 cạnh của tam giác")
